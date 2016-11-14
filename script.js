d56d6c1899359c77f498349e50a4a4387f3a9ca3
larry-kraus <krauslarry@gmail.com>
2016-11-14 12:04:34 -0700

    added gameboard

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("JS linked!");
+window.onload = function () {
+	console.log("JS linked!");
+}
+
+player1Array = ['x', '', '', '', '', '', '', '', '', ''];
+player2Array = ['x', '', '', '', '', '', '', '', '', ''];
