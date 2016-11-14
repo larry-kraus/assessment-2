0178c9c206b9862801cb63258d5ba04dac4c41c9
larry-kraus <krauslarry@gmail.com>
2016-11-14 12:20:38 -0700

    attempting to populate board

--- script.js
+++ script.js
@@ -2,5 +2,33 @@ window.onload = function () {
 	console.log("JS linked!");
 }
 
-player1Array = ['x', '', '', '', '', '', '', '', '', ''];
-player2Array = ['x', '', '', '', '', '', '', '', '', ''];
+row = {
+	x: 10
+}
+
+player1 = {
+	x: 0
+};
+
+player2 = {
+  	x: 0
+};
+
+var updateBoard = function() {
+	
+}
+
+//event listener for 'a'
+var oneMoves = function() {
+	if (player1.x < 10) {
+    	player1.x = player1.x + 1;
+    }
+	updateBoard();
+}
+
+var twoMoves = function() {
+
+
+	updateBoard();
+}
+
