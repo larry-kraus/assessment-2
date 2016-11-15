32e236e137faadbb698c77beb7673d2afaad648b
larry-kraus <krauslarry@gmail.com>
2016-11-15 15:27:43 -0700

    program works, commented and cleaned code

--- script.js
+++ script.js
@@ -1,40 +1,27 @@
 window.onload = function () {
-	console.log("JS linked!");
-//creates the two dots when page has loaded
+//creates the two dots in starting position when page has loaded
 	var block1 = document.getElementById('block1');
 	block1.setAttribute ("class", "dot1");
 
 	var block11 = document.getElementById('block11');
 	block11.setAttribute ("class", "dot2");
 }
-//creating variables for the blocks
-var block2 = document.getElementById('block2');
-var block3 = document.getElementById('block3');
-var block4 = document.getElementById('block4');
-var block5 = document.getElementById('block5');
-var block6 = document.getElementById('block6');
-var block7 = document.getElementById('block7');
-var block8 = document.getElementById('block8');
-var block9 = document.getElementById('block9');
-var block10 = document.getElementById('block10');
 
-// counter
+// counter for player 1
 var currentSpot = 1;
 
 var advanceSpot = function() {
 	return currentSpot ++;
 }
+
+//counter for player 2
 var currentSpot2 = 11;
+
 var advanceSpot2 = function() {
 	return currentSpot2 ++;
 }
 
-
-var move1 = function() {
-	block1.removeAttribute("class", "dot1");
-	block2.setAttribute("class", "dot1");
-}
-
+//this function moves player one
 var moveOne = function(header2) {
 	var blockNum = "block" + currentSpot;
 	var blockNum2 = "block" + (currentSpot + 1);
@@ -46,6 +33,7 @@ var moveOne = function(header2) {
 	winner();
 }
 
+//this function moves player 2
 var moveTwo = function(checkKeyA) {
 	var blockNum3 = "block" + currentSpot2;
 	var blockNum4 = "block" + (currentSpot2 + 1);
@@ -59,15 +47,18 @@ var moveTwo = function(checkKeyA) {
 
 var keycode;
 
+//event listener for player 2
 var aKey = document.addEventListener("keypress", checkKeyA);
 
 function checkKeyA(aKey) {
 	moveTwo();
 }
 
+//event listener for player 1
 var header2 = document.getElementById('dr');
 header2.addEventListener("click", moveOne);
 
+//this function creates an alert for the winner
 var winner = function() {
 	if (currentSpot === 10) {
 		alert("Player 1 wins!!!!");
@@ -80,12 +71,16 @@ var winner = function() {
 	}
 }
 
-
+/* code scrapyard
 var printer = function() {
 	console.log("werks");
 }
 
-/*
+var move1 = function() {
+	block1.removeAttribute("class", "dot1");
+	block2.setAttribute("class", "dot1");
+}
+
 row = {
 	x: 10
 }
@@ -147,5 +142,16 @@ var moveTwo = function(currentSpot) {
 	advanceSpot();
 	console.log("block"+currentSpot);
 }
+
+//creating variables for the blocks
+var block2 = document.getElementById('block2');
+var block3 = document.getElementById('block3');
+var block4 = document.getElementById('block4');
+var block5 = document.getElementById('block5');
+var block6 = document.getElementById('block6');
+var block7 = document.getElementById('block7');
+var block8 = document.getElementById('block8');
+var block9 = document.getElementById('block9');
+var block10 = document.getElementById('block10');
 
 */
