c7c91fb105eaf5291f9f593657925ba69a1563be
larry-kraus <krauslarry@gmail.com>
2016-11-15 14:54:50 -0700

    adds alert for winner

--- script.js
+++ script.js
@@ -24,6 +24,10 @@ var currentSpot = 1;
 var advanceSpot = function() {
 	return currentSpot ++;
 }
+var currentSpot2 = 11;
+var advanceSpot2 = function() {
+	return currentSpot2 ++;
+}
 
 
 var move1 = function() {
@@ -31,47 +35,51 @@ var move1 = function() {
 	block2.setAttribute("class", "dot1");
 }
 
-var move2 = function() {
-	block2.removeAttribute("class", "dot1");
-	block3.setAttribute("class", "dot1");
-}
-
-var move3 = function() {
-	block3.removeAttribute("class", "dot1");
-	block4.setAttribute("class", "dot1");
-}
-
-
-
-var moveTwo = function(header2) {
-	console.log(currentSpot);
+var moveOne = function(header2) {
 	var blockNum = "block" + currentSpot;
 	var blockNum2 = "block" + (currentSpot + 1);
-	console.log(blockNum);
 
 	document.getElementById(blockNum).removeAttribute("class", "dot1");
 	document.getElementById(blockNum2).setAttribute("class", "dot1");
 
 	advanceSpot();
+	winner();
 }
 
-console.log(currentSpot);
+var moveTwo = function(checkKeyA) {
+	var blockNum3 = "block" + currentSpot2;
+	var blockNum4 = "block" + (currentSpot2 + 1);
+
+	document.getElementById(blockNum3).removeAttribute("class", "dot2");
+	document.getElementById(blockNum4).setAttribute("class", "dot2");
+
+	advanceSpot2();
+	winner();
+}
 
 var keycode;
-var key;
 
-document.addEventListener("keypress", checkKeyA);
+var aKey = document.addEventListener("keypress", checkKeyA);
 
-function checkKeyA(key) {
-	console.log(keycode);
-	if (keycode === 97) {
-		console.log("A has been pressed!");
-		//oneMoves();
-	}
+function checkKeyA(aKey) {
+	moveTwo();
 }
 
 var header2 = document.getElementById('dr');
-header2.addEventListener("click", moveTwo);
+header2.addEventListener("click", moveOne);
+
+var winner = function() {
+	if (currentSpot === 10) {
+		alert("Player 1 wins!!!!");
+	}
+	else if (currentSpot2 === 20) {
+		alert("Player 2 wins!!!!");
+	}
+	else {
+		console.log('...not yet');
+	}
+}
+
 
 var printer = function() {
 	console.log("werks");
