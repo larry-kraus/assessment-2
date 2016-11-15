5234d781c195f3839adc2395ce067f5882a20f2b
larry-kraus <krauslarry@gmail.com>
2016-11-15 14:26:36 -0700

    p1 moves all the way across

--- script.js
+++ script.js
@@ -20,12 +20,12 @@ var block10 = document.getElementById('block10');
 
 // counter
 var currentSpot = 1;
-var advanceSpot = function() {
-	currentSpot += 1;
-}
-console.log(currentSpot);
 
-//this baby works, the rest... not so much
+var advanceSpot = function() {
+	return currentSpot ++;
+}
+
+
 var move1 = function() {
 	block1.removeAttribute("class", "dot1");
 	block2.setAttribute("class", "dot1");
@@ -36,36 +36,48 @@ var move2 = function() {
 	block3.setAttribute("class", "dot1");
 }
 
-var mover = function(currentSpot) {
-	if (currentSpot === "1") {
-		move1();
-	}
-	else if (currentSpot === "2") {
-		move2();
-	}
-	else {
-		console.log("idk man");
-	}
+var move3 = function() {
+	block3.removeAttribute("class", "dot1");
+	block4.setAttribute("class", "dot1");
+}
+
+
+
+var moveTwo = function(header2) {
+	console.log(currentSpot);
+	var blockNum = "block" + currentSpot;
+	var blockNum2 = "block" + (currentSpot + 1);
+	console.log(blockNum);
+
+	document.getElementById(blockNum).removeAttribute("class", "dot1");
+	document.getElementById(blockNum2).setAttribute("class", "dot1");
+
 	advanceSpot();
 }
 
-var header2 = document.getElementById('dr');
-header2.addEventListener("click", move1);
-
-
-
 console.log(currentSpot);
+
+var keycode;
+var key;
+
+document.addEventListener("keypress", checkKeyA);
+
+function checkKeyA(key) {
+	console.log(keycode);
+	if (keycode === 97) {
+		console.log("A has been pressed!");
+		//oneMoves();
+	}
+}
+
+var header2 = document.getElementById('dr');
+header2.addEventListener("click", moveTwo);
 
 var printer = function() {
 	console.log("werks");
 }
-/*
-var move2 = function(currentSpot) {
-	(block + currentSpot).removeAttribute("class", "dot1");
-	(block + (currentSpot + 1)).setAttribute("class", "dot1");
-	advanceSpot();
-}
 
+/*
 row = {
 	x: 10
 }
@@ -102,4 +114,30 @@ function checkKeyL(i) {
 		twoMoves();
 	}
 }
+
+var mover = function(currentSpot) {
+	if (currentSpot === 1) {
+		move1();
+	}
+	else if (currentSpot === 2) {
+		move2();
+	}
+	else {
+		console.log("idk man");
+	}
+	advanceSpot();
+}
+
+console.log("block"+currentSpot);
+
+var moveTwo = function(currentSpot) {
+	var blockNum = block + currentSpot;
+	console.log(blockNum);
+
+	blockNum[0].removeAttribute("class", "dot1");
+	("block" + (currentSpot + 1)).setAttribute("class", "dot1");
+	advanceSpot();
+	console.log("block"+currentSpot);
+}
+
 */
