5bbf08986fb2132bf6860a869cdaf779b5f2f270
larry-kraus <krauslarry@gmail.com>
2016-11-14 18:57:42 -0700

    cleaned up JS

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
 window.onload = function () {
 	console.log("JS linked!");
 
-	var startingBlock1 = document.getElementById('block1');
-	startingBlock1.setAttribute ("class", "dot1");
+	var block1 = document.getElementById('block1');
+	block1.setAttribute ("class", "dot1");
 
-	var startingBlock2 = document.getElementById('block11');
-	startingBlock2.setAttribute ("class", "dot2");
+	var block11 = document.getElementById('block11');
+	block11.setAttribute ("class", "dot2");
 }
 
+var move1 = function() {
+	block1.removeAttribute("class", "dot1");
+	block2.setAttribute("class", "dot1");
+}
+
+var header2 = document.getElementById('dr');
+header2.addEventListener("click", move1);
+
+var block2 = document.getElementById('block2');
+var block3 = document.getElementById('block3');
+var block4 = document.getElementById('block4');
+var block5 = document.getElementById('block5');
+var block6 = document.getElementById('block6');
+var block7 = document.getElementById('block7');
+var block8 = document.getElementById('block8');
+var block9 = document.getElementById('block9');
+var block10 = document.getElementById('block10');
+
+
+
+
+
+
+var printer = function() {
+	console.log("werks");
+}
+/*
 row = {
 	x: 10
 }
@@ -23,18 +50,13 @@ player2 = {
 var updateBoard = function() {
 }
 
-var printer = function() {
-	console.log("werks");
+var oneMoves = function() {
+	if (player1.x < 10) {
+    	player1.x = player1.x + 1;
+    }
+	updateBoard();
 }
 
-var header2 = document.getElementById('dr');
-console.log(header2);
-
-header2.addEventListener("click", printer);
-
-
-
-/*
 document.addEventListener("keypress", checkKeyA);
 
 function checkKeyA(keycode) {
@@ -49,18 +71,4 @@ function checkKeyL(i) {
 		twoMoves();
 	}
 }
-
-var oneMoves = function() {
-	if (player1.x < 10) {
-    	player1.x = player1.x + 1;
-    }
-	updateBoard();
-}
-
-var twoMoves = function() {
-
-
-	updateBoard();
-}
-
 */
